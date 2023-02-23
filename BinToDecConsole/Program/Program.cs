f10af73e071280d5449ad55810663f9334720a7c
Alex Duranti <alex.duranti.2006@gmail.com>
2023-02-23 12:04:30 +0100

    messe le variabili

--- BinToDecConsole/Program/Program.cs
+++ BinToDecConsole/Program/Program.cs
@@ -1,12 +1,18 @@
 ﻿using System;
 
-namespace Program
+namespace BinToDecConsole
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int n, lungh, pos=0, pes=0;
+
+            Console.WriteLine("Inserisci il numero binario");
+            string numBin = Console.ReadLine();
+
+            lungh = numBin.Length;
+
         }
     }
 }
